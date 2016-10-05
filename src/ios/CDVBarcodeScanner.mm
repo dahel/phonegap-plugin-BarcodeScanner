7e7cfa986e4fdd2608cf45b98923bd25735db96b
Robert Gacki <r.gacki@gmail.com>
2016-10-05 09:04:42 +0200

    lock orientation for ios

--- src/ios/CDVBarcodeScanner.mm
+++ src/ios/CDVBarcodeScanner.mm
@@ -1070,7 +1070,7 @@ parentViewController:(UIViewController*)parentViewController
 
 - (BOOL)shouldAutorotate
 {
-    return YES;
+    return NO;
 }
 
 - (UIInterfaceOrientation)preferredInterfaceOrientationForPresentation
